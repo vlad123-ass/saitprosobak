2ac4f7c9bf8cda71176ed11b786852a8a3d1fde4
vlad123-ass <bohomolovvlad@gmail.com>
2025-04-06 02:46:32 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
+
 const app = express();
+const PORT = 3000;
 
-// Путь к папке с файлами
-app.use(express.static(path.join(__dirname, 'public')));
+// Токен и chat_id пользователя
+const TELEGRAM_TOKEN = '7319491525:AAHjJngTe50VxcFBr-z9-q_GUqxeSU_J_jY';
+const CHAT_ID = '5289997696';
 
-// Отправка страницы при запросу на корень
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Middleware для отправки IP в Telegram
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const message = `Новый посетитель сайта! IP: ${ip}`;
+
+  axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+    chat_id: CHAT_ID,
+    text: message,
+  }).catch(err => {
+    console.error('Ошибка отправки в Telegram:', err.message);
+  });
+
+  next();
 });
 
-// Запуск сервера
-const PORT = process.env.PORT || 3000;
+// Отдача HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, () => {
-  console.log(`Сервер работает на порту ${PORT}`);
+  console.log(`Сайт работает: http://localhost:${PORT}`);
 });
